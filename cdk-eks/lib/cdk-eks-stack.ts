a3a2a37e0f7d4e3bf5513c48e77162d6172425f7
nnthanh-at-701571471198 <nnthanh@amazon.com>
2020-05-30 21:46:55 +0700

    🚀 cdk-eks >> 2.2.2. cdk-eks-ec2

--- cdk-eks/lib/cdk-eks-stack.ts
+++ cdk-eks/lib/cdk-eks-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 
 import * as dotenv from 'dotenv';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import * as eks from '@aws-cdk/aws-eks';
 
 export class CdkEksStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,6 +17,29 @@ export class CdkEksStack extends cdk.Stack {
      * Step 1. use an existing VPC or create a new one for our EKS Cluster
      */  
     const vpc = getOrCreateVpc(this);
+    
+    /**
+    * Step 2. Create a new EKS Cluster
+    */  
+    
+    // IAM role for our EC2 worker nodes
+    const clusterAdmin = new iam.Role(this, 'EKS-AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
+    });
+
+    var cluster_name = process.env.EKS_CLUSTER_NAME || "EKS-Cluster";
+    // console.log(`cluster_name is ${process.env.EKS_CLUSTER_NAME}`);
+
+    /** Create the cluster 
+    * and a default managed NodeGroup of 2 x m5.large EC2-instances */
+    const cluster = new eks.Cluster(this, cluster_name, {
+      clusterName: cluster_name,
+      vpc,
+      defaultCapacity: 1,
+      mastersRole: clusterAdmin,
+      outputClusterName: true,
+      version: '1.16',
+    });
     
   }
 }
@@ -36,9 +61,17 @@ function getOrCreateVpc(stack: cdk.Stack): ec2.IVpc {
     ec2.Vpc.fromLookup(stack, vpc_name, { isDefault: true }) :
     stack.node.tryGetContext('use_vpc_id') ?
       ec2.Vpc.fromLookup(stack, vpc_name, 
-                         { vpcId: stack.node.tryGetContext('use_vpc_id') }) :
+              { vpcId: stack.node.tryGetContext('use_vpc_id') }) :
       new ec2.Vpc(stack, vpc_name, 
-                         { cidr: vpc_cidr, maxAzs: 2, natGateways: 1 }); 
+              { cidr: vpc_cidr,
+                maxAzs: 2,
+                natGateways: 1,
+                subnetConfiguration: [
+                  {  cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC,  
+                     name: "PublicDMZ"  },
+                  {  cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE, 
+                     name: "PrivateServices" } ]
+              });  
       
   return vpc
 }
